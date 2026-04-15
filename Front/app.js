a91d17a84f0811bf6fed02e483bfaba0247cd250
Maralv01 <alvarezg010831@gmail.com>
2026-04-14 23:06:16 -0500

    Actualizo Archivos

--- Front/app.js
+++ Front/app.js
@@ -1,18 +1,32 @@
 function cambiarTab(tabId, event) {
 
-  // ocultar todo
+ 
   document.querySelectorAll(".tab-content").forEach(el => {
     el.classList.remove("active");
   });
 
-  // quitar activo a tabs
   document.querySelectorAll(".tab").forEach(tab => {
     tab.classList.remove("active");
   });
 
-  // mostrar seleccionado
+  
   document.getElementById(tabId).classList.add("active");
 
-  // activar tab
+ 
   event.target.classList.add("active");
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.body.insertAdjacentHTML('beforeend', data);
+        });
+});
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('nav.html')
+        .then(response => response.text())
+        .then(data => {
+            document.body.insertAdjacentHTML('beforeend', data);
+        });
+});
